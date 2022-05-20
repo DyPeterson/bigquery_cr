a709a7e0f61e73a8465ce4ff83cf24b1987ce969
DyPeterson <dylan.peterson17@gmail.com>
2022-05-20 11:59:37 -0700

    adds doc-string to every function

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ netflix_df = pd.read_csv(netflix_filepath, header=0)
 def csv_to_dataframe(filepath:str, index:str) -> None:
     """
     Load a csv file into a pandas dataframe
+    
     returns inserted csv into 'df' variable
 
-    *args:
-        filepath: string of filepath to csv file
-        index: string of column to index dataframe by
+        - filepath: string of filepath to csv file
+        - index: string of column to index dataframe by
     """
     df = pd.read_csv(filepath, header=0, index_col=[index])
     return df
@@ -20,9 +20,8 @@ def index_setter(dataframe, index):
     """
     Sets the index of dataframe to a desired existing column
 
-    args*:
-        dataframe: dataframe to have index changed
-        index: name of column that index will be set to
+    - dataframe: dataframe to have index changed
+    - index: name of column that index will be set to
     """
     dataframe.set_index(index, inplace=True)
     
@@ -32,48 +31,64 @@ def nullfill(dataframe):
     """    
     dataframe.fillna(value='NULL', axis=1, inplace=True)
 
-def dupllicate_drop(dataframe, col_check=None):
+def duplicate_drop(dataframe, col_list=None):
     """
     drop duplicate rows targeting a specific column(s)
 
-    args*:
-        col_check: ['column1', 'column2']
-            Enter column name as a string
-            Only 1 column required, if none set uses all columns
-            enter multiple columns as strings seperated by commas in a list
-            eg: duplicate drop(dataframe, ['col1', 'col2'])
+    col_list: ['column1', 'column2']
+        - Enter column name as a string
+        - Only 1 column required, if none set uses all columns
+        - enter multiple columns as strings seperated by commas in a list
+        - eg: duplicate drop(dataframe, ['col1', 'col2'])
     """
-    dataframe.drop_duplicates(subset=col_check, inplace=True)
+    dataframe.drop_duplicates(subset=col_list, inplace=True)
 
-def drop_missing_row(dataframe) -> None:
+def drop_missing_row(dataframe, col_list=None) -> None:
     """
-    placeholder
+    Drops dataframe row if it is missing any data in col_list
+
+    col_list: ['column1', 'column2']
+        - Enter column name as a string or list
+        - Only 1 column required, if none set uses all columns
+        - enter multiple columns as strings seperated by commas in a list
+        - eg: duplicate drop(dataframe, ['col1', 'col2'])
     """
-    dataframe.dropna(subset=['type','title'], inplace=True)
+    dataframe.dropna(subset=col_list, inplace=True)
 
 def drop_low_data(dataframe) -> None:
     """
-    Placeholder
+    Drops dataframe row if missing more than 2 values 
     """
     dataframe.dropna(thresh=2, inplace=True)
 
 def drop_extra_columns(dataframe):
     """
-    placeholder
+    Drops dataframe columns if dataframe has more than 12 columns
     """
     dataframe.drop(dataframe.columns[12:])
 
 def write_csv(dataframe):
     """
-    placeholder
+    Writes dataframe into a new csv file located in './data/clean_netflix.csv'
     """
     new_file='./data/clean_netflix.csv'
     dataframe.to_csv(new_file)
 
 def run(dataframe):
+    """
+    Runs collection of defined functions:
+
+    - nullfill
+    - duplicate_drop()
+    - drop_missing_rows()
+    - drop_low_data()
+    - index_setter()
+    - drop_extra_columns()
+    - write_csv()
+    """
     nullfill(dataframe)
-    dupllicate_drop(dataframe)
-    drop_missing_row(dataframe)
+    duplicate_drop(dataframe, 'title')
+    drop_missing_row(dataframe, ['type','title'])
     drop_low_data(dataframe)
     index_setter(dataframe,'title')
     drop_extra_columns(dataframe)
