08830301ee64805e51a744a08ccaa78c8aaf189d
DyPeterson <dylan.peterson17@gmail.com>
2022-05-20 11:06:12 -0700

    add index setter and start a function to remove rows with low data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 netflix_filepath = './data/netflix_titles.csv'
-netflix_df = pd.read_csv(netflix_filepath, header=0, index_col='title')
+netflix_df = pd.read_csv(netflix_filepath, header=0)
 
 def csv_to_dataframe(filepath:str, index:str) -> None:
     """
@@ -13,8 +13,14 @@ def csv_to_dataframe(filepath:str, index:str) -> None:
         filepath: string of filepath to csv file
         index: string of column to index dataframe by
     """
-    df = pd.read_csv(filepath, header=0, index_col=index)
+    df = pd.read_csv(filepath, header=0, index_col=[index])
     return df
+
+def index_setter(dataframe, index):
+    """
+    placeholder
+    """
+    dataframe.set_index(index, inplace=True)
 
 def nullfill(dataframe):
     """
@@ -34,15 +40,29 @@ def drop_missing_row(dataframe) -> None:
     """
     dataframe.dropna(subset=['type','title'], inplace=True)
     
+def drop_low_data(dataframe) -> None:
+    """
+    Placeholder
+    """
+    
 def drop_extra_columns(dataframe):
     """
     placeholder
     """
-    dataframe.drop(dataframe.columns[])
-    
+    dataframe.drop(dataframe.columns[13:])
+
 def write_csv(dataframe):
     """
     placeholder
     """
     new_file='./data/clean_netflix.csv'
-    dataframe.to_csv(new_file,)
+    dataframe.to_csv(new_file)
+
+def run(dataframe):
+    nullfill(dataframe)
+    dupllicate_drop(dataframe)
+    drop_missing_row(dataframe)
+    index_setter(dataframe,'title')
+    drop_extra_columns(dataframe)
+    write_csv(dataframe)
+run(netflix_df)
